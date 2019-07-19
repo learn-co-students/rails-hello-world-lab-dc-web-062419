e49e335b1cc3da4cc595ad00de9675110bb6c564
lesliesage <leslie.sage@gmail.com>
2019-07-18 22:07:31 -0400

    Done.

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+class StaticController < ApplicationController
+  def hello_world
+    # implicit: leave commented out. explicit: comment in.
+    # render 'hello_world', to: 'hello_world'
+  end
+end
